a634645045ebd8a1a0f7ab45e1cc549c8e3b519b
Kevin Blount <kvbchamp@gmail.com>
2020-08-25 12:36:53 -0400

    complete initial database migration

--- custom_user_project/settings.py
+++ custom_user_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'custom_user_app.apps.UsersConfig',
+    'custom_user_app.apps.CustomUserAppConfig',
 ]
 
 MIDDLEWARE = [
