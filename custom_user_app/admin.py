035c371cfcf56e0b08a12ad8c96c948ea8a3a3bb
Kevin Blount <kvbchamp@gmail.com>
2020-08-25 12:33:24 -0400

    register customer user model to Admin portal

--- custom_user_app/admin.py
+++ custom_user_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUserModel
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUserModel
+    field_display_list = ['username', 'displayname']
+
+
+admin.site.register(CustomUserModel, CustomUserAdmin)
