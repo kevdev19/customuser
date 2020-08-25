c949a0e35035f1ced1c6aa29a45e86c8c231d226
Kevin Blount <kvbchamp@gmail.com>
2020-08-25 14:03:24 -0400

    successful test of userlogin and usersignup

--- custom_user_app/urls.py
+++ custom_user_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import SignUpView
 
-urlspatterns = [
-    path('signup/', SignUpView, name='signup')
+urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup')
 ]
--- custom_user_project/urls.py
+++ custom_user_project/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home_html') name='home'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
     path('users/', include('custom_user_app.urls')),
     path('users/', include('django.contrib.auth.urls')),
