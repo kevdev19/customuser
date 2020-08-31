971c81b1cc7dc8ff0fd5fda29f6677c3406f8901
Kevin Blount <kvbchamp@gmail.com>
2020-08-31 12:03:22 -0400

    removed unique=True keyword field attribute for displayname

--- custom_user_app/models.py
+++ custom_user_app/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUserModel(AbstractUser):
-    displayname = models.CharField(max_length=60, unique=True, null=False)
+    # displayname = models.CharField(max_length=60, unique=True, null=False)
+    displayname = models.CharField(max_length=60, null=False)
     age = models.IntegerField(null=True, blank=True)
     homepage = models.URLField(max_length=100, null=True, blank=True)
 
